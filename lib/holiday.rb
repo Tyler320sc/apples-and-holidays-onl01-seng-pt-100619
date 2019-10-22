9eb7cdbeb377cc7038ab88e94263d69e94db7586
Tyler Carroll <tylercarroll1611@gmail.com>
2019-10-22 21:55:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,12 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+    holiday_hash.map do |season, holidays|
+      if supplies.include?("BBQ")
+        event 
+      end 
+    end 
+  end.flatten.compact 
 end
 
 
